e5cffb5a3cefef60fc4e859c537e0cc8f36cb5b3
naveen79111 <51811390+naveen79111@users.noreply.github.com>
2020-07-10 19:25:16 +0530

    Add files via upload

---
+++ sample.php
@@ -0,0 +1,94 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Webpage</title>
+	<link rel="stylesheet" type="text/css" href="CSS.css">
+	<link rel="icon" type="image/x-icon" href="C:\xampp\htdocs\demo\logo.png">
+</head>
+<body>
+	<header>
+	<ul>
+		
+		<li><a href="#Contact">Contact us</a></li>
+		<li><a href="#aboutus">About us</a></li>
+		<li><a href="#services">Services</a></li>
+		<li><a href="#home">Home</a></li>
+	</ul>
+</header>
+	
+		<h1>VYOMA LABS</h1>
+
+
+	
+		<img src="image.jpg" width="900px" height="600px">
+		
+	
+
+	<div>
+		<br>
+		<br>
+		<br>
+		<br>
+
+		<h2>Client Register</h2>
+		<form action="server.php" method="post">
+
+		 <label for="name">Username:</label>
+         <input type="text" name="username" placeholder="naveen">
+         <label for="email">Email</label>
+         <input type="email" name="email">
+         <br>
+         <br>
+         <label for="password">Password</label>
+         <input type="password" name="password" >
+         
+
+         <button type="submit">Register</button>
+</form>
+	</div>
+	<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br> 
+
+	<div class="container">
+		<div class="box">
+			<button>Trainings</button>
+			<p>	We,value labs are ood at trainning</p>
+		</div>
+		<div class="box">
+			<button>Placements</button>
+			<p>We have more client companies so we have good placements for the candidates who have good knowledge and skills</p>
+			
+		</div>
+		<div class="box">
+			<button id=services>Our services</button>
+		</div>
+		
+	</div>
+		<footer>
+
+			<button id="aboutus">About Us</button>
+			<p>
+				We,Value labs are one of the best Consultancy
+			</p>
+		</footer>
+
+
+
+		
+	
+
+</body>
+</html>
